0fc1c9e3630dfbea414e819c085b76d1a6e80c16
stani-and <59360947+stani-and@users.noreply.github.com>
2020-04-07 23:20:29 +0300

    adding the parameters to findVisitorById, editVisitor, editVisitorMenu and showMainMenu

--- projectt/projectt.cpp
+++ projectt/projectt.cpp
@@ -8,23 +8,34 @@ struct BOOK {
     int yearOfRelease = 0;
 };
 
+enum WEEK_DAYS {
+
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN
+};
+
 struct VISITOR {
 
     string firstName = "";
     string lastName = "";
     int age;
     int numberOfBooks = 0;
-    string dayOfVisit = "";
+    WEEK_DAYS dayOfTheWeek;
     BOOK books[100];
 };
 
 //CRUD
 
-int findVisitorById(){}
+int findVisitorById(VISITOR* visitors, int& visitorsCount, int id) {}
 
 void newVisitor(){}
 
-void editVisitor(){}
+void editVisitor(VISITOR* visitors, VISITOR newVisitor, int& visitorsCount, int& maxId) {}
 
 void deleteVisitor(){}
 
@@ -44,11 +55,11 @@ void showVisitorMenu(){}
 
 void deleteVisitorMenu(){}
 
-void editVisitorMenu(){}
+void editVisitorMenu(VISITOR* visitors, int& visitorsCount) {}
 
-void showMainMenu(){}
+bool showMainMenu(VISITOR* visitors, int& visitorCount, int& maxId){}
 
-int main(){}
+int main() {}
 
 
 
