1a5bc886aef0d4a0b5ef5773961746fbc6ab63d7
stani-and <59360947+stani-and@users.noreply.github.com>
2020-04-07 23:37:00 +0300

    adding showReportsMenu function and editing showMainMenu

--- projectt/projectt.cpp
+++ projectt/projectt.cpp
@@ -55,9 +55,66 @@ void showVisitorMenu(){}
 
 void deleteVisitorMenu(){}
 
+void showReportsMenu(){}
+
 void editVisitorMenu(VISITOR* visitors, int& visitorsCount) {}
 
-bool showMainMenu(VISITOR* visitors, int& visitorCount, int& maxId){}
+bool showMainMenu(VISITOR* visitors, int& visitorCount, int& maxId){
+
+    int choice;
+
+    cout << "\nMAIN MENU" << endl;
+    cout << "1. Register a new visitor" << endl;
+    cout << "2. Show all visitors' profiles" << endl;
+    cout << "3. Edit an existing visitor's profile" << endl;
+    cout << "4. Delete an existing visitor's profile" << endl;
+    cout << "5. Reports" << endl;
+    cout << "6. Exit" << endl;
+    cout << "Your choice is: ";
+
+    cin >> choice;
+
+    switch (choice) {
+
+    case 1: {
+
+        newVisitorMenu();
+        break;
+    }
+
+    case 2: {
+
+        showVisitorMenu();
+        break;
+    }
+
+    case 3: {
+
+        editVisitorMenu(visitors, visitorCount);
+        break;
+    }
+
+    case 4: {
+
+        deleteVisitorMenu();
+        break;
+    }
+
+    case 5: {
+
+        showReportsMenu();
+        break;
+    }
+
+    case 6: {
+
+        return false;
+    }
+
+    default: break;
+
+    }
+}
 
 int main() {}
 
