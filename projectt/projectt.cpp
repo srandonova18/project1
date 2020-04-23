516190e68709ce04f20375c0ebec05e03f02b816
stani-and <59360947+stani-and@users.noreply.github.com>
2020-04-24 00:31:11 +0300

    adding chooseDayOfTheWeek(), weekDayToString(), showVisitorMenu(), editVisitorMenu() and fixing newVisitorMenu()

--- projectt/projectt.cpp
+++ projectt/projectt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct BOOK {
@@ -127,20 +128,180 @@ int getAverageOfVisitorsAgesForTheWeek(VISITOR* visitors, int visitorCount) {
 
 //PRESENTATION LAYER
 
-void newVisitorMenu(int Choice) {
-    cout << "This is a new Visitor Menu" << endl;
-    cout << "Fill this to create a new Visitor" << newVisitor;
+WEEK_DAYS chooseDayOfTheWeek() {
 
+    short int day;
 
+    cout << endl;
+    cout << "Choose the day of the visit:" << endl;
+    cout << endl;
+    cout << "0. Monday" << endl;
+    cout << "1. Tuesday" << endl;
+    cout << "2. Wednesday" << endl;
+    cout << "3. Thursday" << endl;
+    cout << "4. Friday" << endl;
+    cout << "5. Saturday" << endl;
+    cout << "6. Sunday" << endl;
+    cout << endl;
+    cout << "Your choice: ";
 
+    cin >> day;
 
+    switch (day) {
 
+        case 0:
+            return WEEK_DAYS::MON;
+            break;
+
+        case 1:
+            return WEEK_DAYS::TUE;
+            break;
+
+        case 2:
+            return WEEK_DAYS::WED;
+            break;
+
+        case 3:
+            return WEEK_DAYS::THU;
+            break;
+
+        case 4:
+            return WEEK_DAYS::FRI;
+            break;
+
+        case 5:
+            return WEEK_DAYS::SAT;
+            break;
+
+        case 6:
+            return WEEK_DAYS::SUN;
+            break;
+
+        default:
+            break;
+    }
 
 }
-void showVisitorMenu(){}
 
-void deleteVisitorMenu(VISITOR* visitors, int& visitorCount, int& maxId)
-{
+
+string weekDayToString(WEEK_DAYS weekDay) {
+
+    switch (weekDay) {
+
+        case WEEK_DAYS::MON:
+            return "Monday";
+            break;
+
+        case WEEK_DAYS::TUE:
+            return "Tuesday";
+            break;
+
+        case WEEK_DAYS::WED:
+            return "Wednesday";
+            break;
+
+        case WEEK_DAYS::THU:
+            return "Thursday";
+            break;
+
+        case WEEK_DAYS::FRI:
+            return "Friday";
+            break;
+
+        case WEEK_DAYS::SAT:
+            return "Saturday";
+            break;
+
+        case WEEK_DAYS::SUN:
+            return "Sunday";
+            break;
+
+        default:
+            break;
+    }
+
+    return " ";
+
+}
+
+void newVisitorMenu(VISITOR* visitors, int& visitorCount, int& maxId) {
+   
+    VISITOR visitor;
+    short int day;
+
+    cin.ignore();
+
+    cout << "First name: ";
+    getline(cin, visitor.firstName);
+
+    cout << "Last name: ";
+    getline(cin, visitor.lastName);
+
+    cout << "Age: ";
+    cin >> visitor.age;
+
+    visitor.dayOfTheWeek = chooseDayOfTheWeek();
+
+    cout << endl;
+    cout << "How many books did they take? : ";
+    cin >> visitor.numberOfBooks;
+
+    cout << endl;
+
+    cout << ".::BOOKS::." << endl;
+
+    cout << endl;
+
+    cin.ignore();
+
+    for (int i = 0; i < visitor.numberOfBooks; i++) {
+
+        cout << "Title: ";
+        getline(cin, visitor.books[i].title);
+
+        cout << "Author: ";
+        getline(cin, visitor.books[i].authorName);
+
+        cout << "Year of release: ";
+        cin >> visitor.books[i].yearOfRelease;
+
+        cin.ignore();
+
+        cout << endl;
+    }
+
+    newVisitor(visitors, visitorCount, visitor, maxId);
+}
+
+void showVisitorMenu(VISITOR* visitors, int& visitorCount, int& maxId){
+
+    cout << ".::All VISITORS::. " << endl;
+
+    cout << endl;
+
+    for (int i = 0; i < visitorCount; i++) {
+
+        cout << i + 1 << ". " << visitors[i].firstName << " " << visitors[i].lastName << ", ";
+        cout << visitors[i].age << " years old ";
+        cout << "visited on " << weekDayToString(visitors[i].dayOfTheWeek) << endl;
+
+        cout << endl;
+        cout << ".::LIST OF BOOKS TAKEN::." << endl;
+        cout << endl;
+
+        for (int j = 0; j < visitors[i].numberOfBooks; j++) {
+
+            cout << "\"" << visitors[i].books[j].title << "\" by " << visitors[i].books[j].authorName;
+            cout << " released in " << visitors[i].books[j].yearOfRelease << endl;
+        }
+
+        cout << endl;
+    }
+
+}
+
+void deleteVisitorMenu(VISITOR* visitors, int& visitorCount, int& maxId) {
+
     int visitorId;
 
     cout<<"Enter visitor ID: ";
@@ -151,7 +312,110 @@ void deleteVisitorMenu(VISITOR* visitors, int& visitorCount, int& maxId)
 
 void showReportsMenu(){}
 
-void editVisitorMenu(VISITOR* visitors, int& visitorCount) {}
+void editVisitorMenu(VISITOR* visitors, int& visitorCount) {
+
+    int visitorId, bookIndex;
+    short int edit, bookDetail;
+
+    cout << "Enter visitor's ID: ";
+    cin >> visitorId;
+
+    VISITOR visitor = findVisitor(visitors, visitorCount, visitorId);
+
+    cout << "Choose what you want to edit: " << endl;
+    cout << endl;
+    cout << "1. First name" << endl;
+    cout << "2. Last name" << endl;
+    cout << "3. Age" << endl;
+    cout << "4. Day of visit" << endl;
+    cout << "5. Any of the books' details" << endl;
+    cout << endl;
+    cout << "Your choice: ";
+
+    cin >> edit;
+
+    cin.ignore();
+
+    switch (edit) {
+
+        case 1:
+            cout << "First name: ";
+            getline(cin, visitor.firstName);
+            editVisitor(visitors, visitor, visitorCount, visitorId);
+            break;
+
+        case 2:
+            cout << "Last name: ";
+            getline(cin, visitor.lastName);
+            editVisitor(visitors, visitor, visitorCount, visitorId);
+            break;
+
+        case 3:
+            cout << "Age: ";
+            cin >> visitor.age;
+            editVisitor(visitors, visitor, visitorCount, visitorId);
+            break;
+
+        case 4:
+            visitor.dayOfTheWeek = chooseDayOfTheWeek();
+            editVisitor(visitors, visitor, visitorCount, visitorId);
+            break;
+
+        case 5:
+            cout << "Choose a book: " << endl;
+            cout << endl;
+
+            for (int i = 0; i < visitor.numberOfBooks; i++) {
+
+                cout << i + 1 << ".\" " << visitor.books[i].title << "\" by ";
+                cout << visitor.books[i].authorName << " released in " << visitor.books[i].yearOfRelease << endl;
+            }
+
+            cin >> bookIndex;
+            bookIndex--;
+
+            cout << "Choose the detail you want to edit:" << endl;
+            cout << endl;
+            cout << "1. Title" << endl;
+            cout << "2. Author" << endl;
+            cout << "3. Year of release" << endl;
+            cout << endl;
+            cout << "Your choice: ";
+
+            cin >> bookDetail;
+
+            cout << endl;
+
+            cin.ignore();
+
+            switch (bookDetail) {
+
+                case 1:
+                    cout << "Title: ";
+                    getline(cin, visitor.books[bookIndex].title);
+                    editVisitor(visitors, visitor, visitorCount, visitorId);
+                    break;
+
+                case 2:
+                    cout << "Author: ";
+                    getline(cin, visitor.books[bookIndex].authorName);
+                    editVisitor(visitors, visitor, visitorCount, visitorId);
+                    break;
+
+                case 3:
+                    cout << "Year of release: ";
+                    cin >> visitor.books[bookIndex].yearOfRelease;
+                    editVisitor(visitors, visitor, visitorCount, visitorId);
+                    break;
+
+                default: break;
+            }
+
+        default: break;
+    }
+
+
+}
 
 bool showMainMenu(VISITOR* visitors, int& visitorCount, int& maxId){
 
