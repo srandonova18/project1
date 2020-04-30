03f4e6464f01e6a707bc9991f1cc0fa0220f36e4
stani-and <59360947+stani-and@users.noreply.github.com>
2020-04-30 08:53:42 +0300

    create header file for data types

---
+++ projectt/StructuresAndEnum.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+using namespace std;
+
+//structure with information about the books
+struct BOOK {
+
+	string title = "";
+	string authorName = "";
+	short int yearOfRelease = 0;
+};
+
+//declaring the enum with the days of the week
+enum class WEEK_DAYS {
+
+	UNKNOWN,
+	MON,
+	TUE,
+	WED,
+	THU,
+	FRI,
+	SAT,
+	SUN
+};
+
+//structure with information about the visitors
+struct VISITOR {
+
+	string firstName = "";
+	string lastName = "";
+	short int age = 0;
+	WEEK_DAYS dayOfTheWeek = WEEK_DAYS::MON;
+	short int numberOfBooks = 0;
+	BOOK books[7];
+	int id = 0;
+};
